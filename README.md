570175da18a64213d57d51e3292118aecbc55061
czM1K3 <misamadera1@gmail.com>
2021-11-15 21:54:52 +0100

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # prisma-redis
+
+This package is middleware for Prisma that caches requests to Redis.
+Based on the work done by @Asjas on [prisma-redis-middleware](https://github.com/Asjas/prisma-redis-middleware)
+
+This package requires [ioredis](https://www.npmjs.com/package/ioredis)
+
+## Installation
+```bash
+npm i prisma-redis
+```
+or
+```base
+yarn add prisma-redis
+```
+
+## Implementation
+```ts
+import { PrismaClient } from "@prisma/client";
+import { createPrismaRedisCache } from "prisma-redis";
+import Redis from "ioredis";
+
+const prisma = new PrismaClient();
+
+const redis = new Redis();
+prisma.$use(createPrismaRedisCache(["Objects","to","cache"], 60, redis));
+
+...
+```
+In 4th argument you can define which command to cache.
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import Redis from "ioredis";
+import {parse, stringify} from "telejson";
+
+type CacheMethod = "findUnique"|"findFirst"|"findMany"|"queryRaw"|"aggregate"|"count"|"groupBy";
+
+export const createPrismaRedisCache = (model: string[], cacheTime: number, redis: Redis.Redis, cacheMethods?: CacheMethod[]) => {
+	return async function prismaCacheMiddleware(params: any, next: (params: any) => Promise<any>) {
+		if (
+			model.includes(params.model) &&
+			cacheMethods ? cacheMethods.includes(params.actions) : ["findUnique", "findFirst", "findMany", "queryRaw", "aggregate", "count", "groupBy"].includes(params.action)
+		) {
+			const args = stringify(params.args);
+console.log(params);
+			const cacheKey = `${params.model}_${params.action}_${args}`;
+
+			let result = parse((await redis.get(cacheKey))!);
+			console.log(result);
+
+			if (!result) {
+				result = await next(params);
+				await redis.setex(cacheKey, cacheTime, stringify(result));
+				console.log(result);
+			}
+			return result;
+		}
+		else {
+			return await next(params);
+		}
+	}
+}
